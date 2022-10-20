d4b1027e125aaf26b8f479df46d11fdb790a2b9a
salientarc <nikhildhiman9050@gmail.com>
2022-10-20 21:07:24 +0530

    updated code

---
+++ assets/js/main.js
@@ -0,0 +1,210 @@
+(function () {
+  "use strict";
+
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  const scrollto = (el) => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+
+  on(
+    "click",
+    "#navbar .nav-link",
+    function (e) {
+      let section = select(this.hash);
+      if (section) {
+        e.preventDefault();
+
+        let navbar = select("#navbar");
+        let header = select("#header");
+        let sections = select("section", true);
+        let navlinks = select("#navbar .nav-link", true);
+
+        navlinks.forEach((item) => {
+          item.classList.remove("active");
+        });
+
+        this.classList.add("active");
+
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+
+        if (this.hash == "#header") {
+          header.classList.remove("header-top");
+          sections.forEach((item) => {
+            item.classList.remove("section-show");
+          });
+          return;
+        }
+
+        if (!header.classList.contains("header-top")) {
+          header.classList.add("header-top");
+          setTimeout(function () {
+            sections.forEach((item) => {
+              item.classList.remove("section-show");
+            });
+            section.classList.add("section-show");
+          }, 350);
+        } else {
+          sections.forEach((item) => {
+            item.classList.remove("section-show");
+          });
+          section.classList.add("section-show");
+        }
+
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
+
+  window.addEventListener("load", () => {
+    if (window.location.hash) {
+      let initial_nav = select(window.location.hash);
+
+      if (initial_nav) {
+        let header = select("#header");
+        let navlinks = select("#navbar .nav-link", true);
+
+        header.classList.add("header-top");
+
+        navlinks.forEach((item) => {
+          if (item.getAttribute("href") == window.location.hash) {
+            item.classList.add("active");
+          } else {
+            item.classList.remove("active");
+          }
+        });
+
+        setTimeout(function () {
+          initial_nav.classList.add("section-show");
+        }, 350);
+
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+  let skilsContent = select(".skills-content");
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: "80%",
+      handler: function (direction) {
+        let progress = select(".progress .progress-bar", true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute("aria-valuenow") + "%";
+        });
+      },
+    });
+  }
+})();
+
+// TextScramble
+
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = "!<>-_\\/[]{}—=+*^?#________";
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = "";
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
+const phrases = [
+  "a Student",
+  "a Web Developer",
+  "a Cybersec Enthusiast",
+  "an OpenSource Enthusiast",
+  "a Gamer",
+];
+
+const el = document.querySelector(".anim-text");
+const fx = new TextScramble(el);
+
+let counter = 0;
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 800);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+
+next();
